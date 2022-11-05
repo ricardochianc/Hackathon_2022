7f1d82f9c853b87d7a6637f630b76327b3caaa98
Ricardo Chian <ricardochianc@gmail.com>
2022-11-05 14:48:08 -0600

    Se ha realizado el primer ejercicio

---
+++ Hackathon_2022_Chian_Rodriguez/Hackathon_2022_Chian_Rodriguez/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hackathon_2022_Chian_Rodriguez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hackathon 2022 | Alex Rodriguez, Ricardo Chian");
+
+            Console.WriteLine("Ingrese una palabra: ");
+            var palabraOriginal = Console.ReadLine();
+
+            var Scramble = new Scramble(palabraOriginal);
+
+            Console.WriteLine("Ingrese otra palabra: ");
+            var palabraComparar = Console.ReadLine();
+
+            if (Scramble.verificarSimilar(palabraComparar))
+            {
+                Console.WriteLine("La segunda cadena es una cadena desordenada de la primera");
+            }
+            else
+            {
+                Console.WriteLine("La segunda cadena NO es una cadena desordenada de la primera");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hackathon_2022_Chian_Rodriguez/Hackathon_2022_Chian_Rodriguez/Scramble.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon_2022_Chian_Rodriguez
+{
+    public class Scramble
+    {
+        public Dictionary<char, int> PalabraOriginal { get; set; }
+
+        public Scramble(string palabraOriginal)
+        {
+            PalabraOriginal = new Dictionary<char, int>();
+
+            foreach (var letra in palabraOriginal.ToUpper())
+            {
+                if (!PalabraOriginal.ContainsKey(letra))
+                {
+                    PalabraOriginal.Add(letra, palabraOriginal.Count(c => (c == letra)));
+                }
+            }
+        }
+
+        public bool verificarSimilar(string palabraComparar)
+        {
+            var esCorrecto = true;
+            
+            foreach (var letra in palabraComparar.ToUpper())
+            {
+                esCorrecto = PalabraOriginal.ContainsKey(letra);
+
+                if (!esCorrecto)
+                {
+                    return esCorrecto;
+                }
+            }
+            return esCorrecto;
+        }
+    }
+}
