103877e6dbc8bcbf3b51953bb6b075f13f680ac9
Ricardo Chian <ricardochianc@gmail.com>
2022-11-05 16:46:53 -0600

    Se termino el segundo problema

--- Hackathon_2022_Chian_Rodriguez/Hackathon_2022_Chian_Rodriguez/Program.cs
+++ Hackathon_2022_Chian_Rodriguez/Hackathon_2022_Chian_Rodriguez/Program.cs
@@ -6,7 +6,10 @@ namespace Hackathon_2022_Chian_Rodriguez
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hackathon 2022 | Alex Rodriguez, Ricardo Chian");
+
+            //PROBLEMA 1
 
             Console.WriteLine("Ingrese una palabra: ");
             var palabraOriginal = Console.ReadLine();
@@ -24,6 +27,31 @@ namespace Hackathon_2022_Chian_Rodriguez
             {
                 Console.WriteLine("La segunda cadena NO es una cadena desordenada de la primera");
             }
+
+            //PROBLEMA 2
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Problema 1");
+            Console.WriteLine("Ingrese la longitud de las lineas");
+            var longitudStr = Console.ReadLine();
+
+            Console.WriteLine("Ingrese una oración:");
+            var oracion = Console.ReadLine();
+
+            var justificador = new justificador(Int32.Parse(longitudStr), oracion);
+            justificador.justificar();
+
+            if (justificador.CantidadCorrecta == false)
+            {
+                Console.WriteLine("\n\n");
+                Console.WriteLine("La cantidad de caracteres permitidos en una linea es menor a una de las palabras ingresadas, la nueva longitud es de: " );
+                Console.WriteLine(justificador.CaracteresLinea.ToString());
+            }
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine(justificador.TextoJustificado);
+
+
 
             Console.ReadKey();
         }
---
+++ Hackathon_2022_Chian_Rodriguez/Hackathon_2022_Chian_Rodriguez/justificador.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon_2022_Chian_Rodriguez
+{
+    public class justificador
+    {
+        public int CaracteresLinea { get; set; }
+
+        public List<string> Palabras { get; set; }
+
+        public List<string> LineasParrafo { get; set; }
+
+        public string TextoJustificado { get; set; }
+
+        public bool CantidadCorrecta { get; set; }
+
+        public justificador(int caracteresLinea, string oracion)
+        {
+            CaracteresLinea = caracteresLinea;
+            Palabras = oracion.Split(" ").ToList();
+            CantidadCorrecta = true;
+
+            foreach (var palabra in Palabras)
+            {
+                if (palabra.Length > CaracteresLinea)
+                {
+                    CantidadCorrecta = false;
+                    CaracteresLinea = palabra.Length + 1;
+                }
+            }
+
+            LineasParrafo = new List<string>();
+            TextoJustificado = "";
+        }
+
+        public void justificar()
+        {
+            var lineaActual = ""; //
+
+            while (Palabras.Count > 0)
+            {
+                var top = Palabras.ElementAt(0); //Palabra en el top de la lista
+
+                if ((lineaActual.Length < CaracteresLinea) && ((top.Length + lineaActual.Length) < CaracteresLinea))
+                {
+                    lineaActual += top + " ";
+                    Palabras.RemoveAt(0);
+                }
+                else
+                {
+                    LineasParrafo.Add(rellenar(lineaActual));
+                    lineaActual = "";
+                }
+            }
+
+            LineasParrafo.Add(rellenar(lineaActual));
+            TextoJustificado = String.Join("\n", LineasParrafo);
+        }
+
+        public string rellenar(string linea)
+        {
+            if (linea != "")
+            {
+                var lineaActual = "";
+                var palabras = linea.Split(" ").ToList(); //This is an
+                var posicion = 0;
+
+                palabras.RemoveAt(palabras.Count - 1);
+
+                while (lineaActual.Length < CaracteresLinea)
+                {
+                    lineaActual = "";
+
+                    var p = palabras.ElementAt(posicion);
+                    p += " ";
+                    palabras[posicion] = p;
+                    posicion++;
+
+                    if ((posicion + 1) >= palabras.Count)
+                    {
+                        posicion = 0;
+                    }
+
+                    lineaActual = String.Join("", palabras);
+                }
+                return lineaActual;
+            }
+
+            return "";
+        }
+    }
+}
